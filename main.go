5b727015e6fe9582c853f100913b8fe7b9a971a3
Maximilian Welsch <maxwelsch93@gmail.com>
2024-01-12 20:36:21 +0100

    add named templates and styling

--- main.go
+++ main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/welschma/gowebdev/controllers"
+	"github.com/welschma/gowebdev/templates"
 	"github.com/welschma/gowebdev/views"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
-	r.Get("/faq", controllers.StaticHandler(tpl))
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))
+	r.Get("/faq", controllers.FAQ(tpl))
 
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
--- views/template.go
+++ views/template.go
@@ -3,6 +3,7 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
@@ -11,7 +12,7 @@ type Template struct {
 	htmlTpl *template.Template
 }
 
-func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+func (t Template) Execute(w http.ResponseWriter, data any) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	err := t.htmlTpl.Execute(w, data)
@@ -25,6 +26,16 @@ func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 
 func Parse(filepath string) (Template, error) {
 	tpl, err := template.ParseFiles(filepath)
+
+	if err != nil {
+		return Template{}, fmt.Errorf("error parsing template: %w", err)
+	}
+
+	return Template{htmlTpl: tpl}, nil
+}
+
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl, err := template.ParseFS(fs, patterns...)
 
 	if err != nil {
 		return Template{}, fmt.Errorf("error parsing template: %w", err)
