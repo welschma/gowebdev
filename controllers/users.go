d31a8ed18656fc5e783ea8539b22cc8dbde7cd6d
Maximilian Welsch <maxwelsch93@gmail.com>
2024-03-12 08:31:08 +0100

    finish authetication

--- controllers/users.go
+++ controllers/users.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/welschma/gowebdev/context"
 	"github.com/welschma/gowebdev/models"
@@ -12,9 +13,14 @@ type Users struct {
 	Templates struct {
 		New Template
 		SignIn Template
+        ForgotPassword Template
+        CheckYourEmail Template
+        ResetPassword Template
 	}
 	UserService *models.UserService
     SessionService *models.SessionService
+    PasswordResetService *models.PasswordResetService
+    EmailService *models.EmailService
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
@@ -108,11 +114,86 @@ func (u Users) ProcessSignOut(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
 //SetUser and RequireUser middleware are required
 func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
     user := context.User(r.Context())
     fmt.Fprintf(w, "Current user: %s\n", user.Email)
+}
+
+func (u Users) ForgotPassword(w http.ResponseWriter, r*http.Request) {
+    var data struct {
+        Email string
+    }
+    data.Email = r.FormValue("email")
+    u.Templates.ForgotPassword.Execute(w, r, data)
+}
+
+func (u Users) ProcessForgotPassword(w http.ResponseWriter, r*http.Request) {
+    var data struct {
+        Email string
+    }
+    data.Email = r.FormValue("email")
+
+    pwReset, err := u.PasswordResetService.Create(data.Email)
+    if err != nil {
+        fmt.Println(err)
+        http.Error(w, "Something went wrong", http.StatusInternalServerError)
+        return
+    }
+
+    vals := url.Values{
+        "token":  {pwReset.Token},
+    }
+
+    //TODO: Make url configurable
+    resetUrl := "localhost:3000/reset-pw?" + vals.Encode()
+    err = u.EmailService.ForgotPassword(data.Email, resetUrl)
+    if err != nil {
+        fmt.Println(err)
+        http.Error(w, "Something went wrong", http.StatusInternalServerError)
+        return
+    }
+
+    u.Templates.CheckYourEmail.Execute(w, r, data)
+}
+
+func (u Users) ResetPassword(w http.ResponseWriter, r*http.Request) {
+    var data struct {
+        Token string
+    }
+    data.Token = r.FormValue("token")
+    u.Templates.ResetPassword.Execute(w, r, data)
+}
+
+func (u Users) ProcessResetPassword(w http.ResponseWriter, r*http.Request) {
+    var data struct {
+        Token string
+        Password string
+    }
+    data.Token = r.FormValue("token")
+    data.Password = r.FormValue("password")
+
+    user, err := u.PasswordResetService.Consume(data.Token)
+    if err != nil {
+        fmt.Println(err)
+        http.Error(w, "something went wrong.", http.StatusInternalServerError)
+    }
+
+    err = u.UserService.UpdatePassword(user.ID, data.Password)
+    if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+
+    session, err := u.SessionService.Create(user.ID)
+    if err != nil {
+        fmt.Println(err)
+        http.Redirect(w, r, "/signin", http.StatusFound)
+    }
+
+    setCookie(w, CookieSession, session.Token)
+    http.Redirect(w, r, "/users/me", http.StatusFound)
 }
 
 
--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/csrf"
+	"github.com/joho/godotenv"
 	"github.com/welschma/gowebdev/controllers"
 	"github.com/welschma/gowebdev/migrations"
 	"github.com/welschma/gowebdev/models"
@@ -21,11 +24,53 @@ func RequestLoggerMw(h http.Handler) http.Handler {
 	})
 }
 
-func main() {
+type config struct {
+	PSQL models.PostgresConfig
+	SMTP models.SMTPConfig
+	CSRF struct {
+		Key    string
+		Secure bool
+	}
+	Server struct {
+		Address string
+	}
+}
 
+func loadEnvConfig() (config, error) {
+	var cfg config
+	err := godotenv.Load()
+	if err != nil {
+		return cfg, err
+	}
+	cfg.PSQL = models.DefaultPostgresConfig()
+
+	cfg.SMTP.Host = os.Getenv("SMTP_HOST")
+	portStr := os.Getenv("SMTP_PORT")
+	cfg.SMTP.Port, err = strconv.Atoi(portStr)
+	if err != nil {
+		return cfg, err
+	}
+	cfg.SMTP.Username = os.Getenv("SMTP_USERNAME")
+	cfg.SMTP.Password = os.Getenv("SMTP_PASSWORD")
+
+	// TODO: Read the CSRF values from an ENV variable
+	cfg.CSRF.Key = "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
+	cfg.CSRF.Secure = false
+
+	// TODO: Read the server values from an ENV variable
+	cfg.Server.Address = ":3000"
+
+	return cfg, nil
+}
+
+func main() {
+	cfg, err := loadEnvConfig()
+
+	if err != nil {
+		panic(err)
+	}
 	//Set up the database connection
-	cfg := models.DefaultPostgresConfig()
-	db, err := models.Open(cfg)
+	db, err := models.Open(cfg.PSQL)
 	if err != nil {
 		panic(err)
 	}
@@ -38,40 +83,49 @@ func main() {
 	}
 
 	//Set up services
-	userService := models.UserService{
+	userService := &models.UserService{
 		DB: db,
 	}
 
-	sessionService := models.SessionService{
+	sessionService := &models.SessionService{
 		DB: db,
 	}
+
+	pwResetService := &models.PasswordResetService{
+		DB: db,
+	}
+	emailService := models.NewEmailService(cfg.SMTP)
 
 	//Set up middleware
 	umw := controllers.UserMiddleware{
-		SessionService: &sessionService,
+		SessionService: sessionService,
 	}
 
-	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
 	csrfMw := csrf.Protect(
-		[]byte(csrfKey),
-		csrf.Secure(false),
+		[]byte(cfg.CSRF.Key),
+		csrf.Secure(cfg.CSRF.Secure),
 	)
 
 	//Set up controllers
 	usersC := controllers.Users{
-		UserService:    &userService,
-		SessionService: &sessionService,
+		UserService:    userService,
+		SessionService: sessionService,
+		PasswordResetService: pwResetService,
+		EmailService: emailService,
 	}
 
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
+	usersC.Templates.ForgotPassword = views.Must(views.ParseFS(templates.FS, "forgot-pw.gohtml", "tailwind.gohtml"))
+	usersC.Templates.CheckYourEmail = views.Must(views.ParseFS(templates.FS, "check-your-email.gohtml", "tailwind.gohtml"))
+	usersC.Templates.ResetPassword = views.Must(views.ParseFS(templates.FS, "reset-pw.gohtml", "tailwind.gohtml"))
 
 	//Set up router and routes
 	r := chi.NewRouter()
-    
-    r.Use(RequestLoggerMw)
-    r.Use(csrfMw)
-    r.Use(umw.SetUser)
+
+	r.Use(RequestLoggerMw)
+	r.Use(csrfMw)
+	r.Use(umw.SetUser)
 
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 
@@ -87,17 +141,22 @@ func main() {
 
 	r.Post("/signout", usersC.ProcessSignOut)
 
-    r.Route("/users/me", func(r chi.Router) {
-        r.Use(umw.RequireUser)
-        r.Get("/", usersC.CurrentUser)
-    })
+	r.Get("/forgot-pw", usersC.ForgotPassword)
+	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
 
+	r.Get("/reset-pw", usersC.ResetPassword)
+	r.Post("/reset-pw", usersC.ProcessResetPassword)
 
-    r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-        http.Error(w, "Page not found", http.StatusNotFound)
-    })
+	r.Route("/users/me", func(r chi.Router) {
+		r.Use(umw.RequireUser)
+		r.Get("/", usersC.CurrentUser)
+	})
 
-    //Start the server
-	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", r)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+
+	//Start the server
+	fmt.Printf("Starting the server on %s...\n", cfg.Server.Address)
+	http.ListenAndServe(cfg.Server.Address, r)
 }
